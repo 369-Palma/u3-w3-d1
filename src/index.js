9ecd6375219944f590222d8765048319b88ba40f
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-02-21 18:44:55 +0100

    finito

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import { Provider } from 'react-redux'
-import store from './redux/store'
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+import { store, persistor } from "./redux/store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
-)
+);
--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,14 +2,29 @@ import { configureStore, combineReducers } from "@reduxjs/toolkit";
 /* import mainReducer from '../reducers' */
 import favouritesReducer from "../reducers/favsReducer";
 import dataReducer from "../reducers/data";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { encryptTransform } from "redux-persist-transform-encrypt";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  transforms: [
+    encryptTransform({
+      secretKey: process.env.REACT_APP_PERSIST_KEY,
+    }),
+  ],
+};
 
 const mainReducer = combineReducers({
   favs: favouritesReducer,
   data: dataReducer,
 });
 
-const store = configureStore({
-  reducer: mainReducer,
+const persistedReducer = persistReducer(persistConfig, mainReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
 });
 
-export default store;
+export const persistor = persistStore(store);
